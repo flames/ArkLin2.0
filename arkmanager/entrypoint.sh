f483f9d24903a4d9e23c69b811004fdde400b62d
Arthur Mayer <mayer.arthur@gmail.com>
2023-06-12 19:23:51 +0000

    steamcmd small fix

--- arkmanager/entrypoint.sh
+++ arkmanager/entrypoint.sh
@@ -6,9 +6,6 @@ printf "\n\033[0;44m---> Starting the SSH server.\033[0m\n"
 service ssh start
 service ssh status
 
-# installation as root
-steamcmd +quit
-
 # installation as steam
 su -c 'ln -sf /usr/games/steamcmd /home/steam/Steam/steamcmd' steam
 su -c '/home/steam/Steam/steamcmd +quit' steam
